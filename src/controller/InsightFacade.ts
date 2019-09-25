664f46e766f914d17ef64a06a30c1643745e6962
Al-Parillon <alparillon@gmail.com>
2019-09-24 17:48:41 -0700

    Entered Validity Error Checking Add Datasets

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -14,9 +14,35 @@ export default class InsightFacade implements IInsightFacade {
     }
 
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+        // TODO: AL - Test whether string is blankspace, field is underscored or ID exists (COMPLETED)
+        if (!(this.isValidId(id))) {
+            throw new InsightError();
+        }
+        // DONE
+        // TODO: Al - Figure out how to parse the dataset
+        // NOT DONE
         return Promise.reject("Not implemented.");
     }
+    private isValidId(id: string): boolean {
+        // Tests whether or not ID provided is valid
+        if (id === " ") {
+            return false;
+        }
+        if (id.includes("_")) {
+            return false;
+        }
+        this.listDatasets().then((datasets: InsightDataset[]) => {
+            for (const dset of datasets) {
+                if (dset.id === id) {
+                    return false;
+                }
+            }
+        }).catch((err: any) => {
+            return false;
+        });
 
+        return true;
+    }
     public removeDataset(id: string): Promise<string> {
         return Promise.reject("Not implemented.");
     }
