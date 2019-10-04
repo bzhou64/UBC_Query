5468153792cb3b74080ca159d0799a3c6f5363bd
raghavaggsss <zacefron.efron727@gmail.com>
2019-10-04 13:23:28 -0700

    split addDataset into helpers

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -28,9 +28,7 @@ export default class InsightFacade implements IInsightFacade {
                     throw new InsightError("Cannot read from data directory");
                 }
                 filenames.forEach((filename) => {
-                    let fileContent = fs.readFileSync(this.dataDir + filename, "utf8");
-                    let currDatasetRead: DataSet = JSON.parse(fileContent);
-                    this.datasets.addDataset(currDatasetRead);
+                    this.loadDatasetDisk(filename);
                 });
                 if (!this.isIDValid(id)) {
                     throw new InsightError("Invalid Id");
@@ -42,49 +40,19 @@ export default class InsightFacade implements IInsightFacade {
                         let currDataset: DataSet = new DataSet(id);
                         let zipFile = new JSZip();
                         zipFile.loadAsync(content, {base64: true}).then((data) => {
-                                let promisesFiles: any[] = [];
-                                for (let file in data.files) {
-                                    if (file.length > "courses/".length && file.substring(0, 8) === "courses/") {
-                                        let currFilePromise = data.files[file].async("text")
-                                            .then((courseData: any) => {
-                                                return JSON.parse(courseData);
-                                            })
-                                            .catch((errZip: any) => {
-                                                reject(new InsightError("JSZip cannot parse the contents of a file"));
-                                            });
-                                        promisesFiles.push(currFilePromise);
-                                    }
-                                }
+                                let promisesFiles: any[] = this.createFileReadPromises(data);
                                 Promise.all(promisesFiles).then((filesJSON) => {
                                     filesJSON.forEach((fileJSON: any) => {
                                         let sections: any[] = fileJSON["result"];
                                         for (let section of sections) {
-                                            if (section.Subject && section.Course &&
-                                                section.Avg  && section.Professor && section.Title
-                                                && section.Pass && section.Fail && section.Audit
-                                                && section.id  && section.Year) {
-                                                let sectionObject: Section = new Section(section.Subject,
-                                                    section.Course, section.Avg,
-                                                    section.Professor, section.Title, section.Pass,
-                                                    section.Fail, section.Audit, section.id,
-                                                    section.Year);
-                                                currDataset.addSection(sectionObject);
+                                            let sectionObj: Section = this.createValidSection(section);
+                                            if (sectionObj) {
+                                                currDataset.addSection(sectionObj);
                                             }
                                         }
                                     });
                                     if (Object.keys(currDataset.sections).length) {
-                                        this.datasets.addDataset(currDataset);
-                                        let my: any = this.datasets.datasets["courses"].sections;
-                                        Log.trace(Object.keys(my).length);
-                                        try {
-                                            if (!fs.existsSync(this.dataDir)) {
-                                                fs.mkdirSync(this.dataDir);
-                                            }
-                                            fs.writeFileSync(this.dataDir + currDataset.id,
-                                                JSON.stringify(currDataset));
-                                        } catch (e) {
-                                            throw new InsightError("Cannot write to disk");
-                                        }
+                                        this.addDatasetDisk(currDataset);
                                         resolve(Object.keys(this.datasets.datasets));
                                     } else {
                                         throw new InsightError("No valid section in Zip File");
@@ -104,13 +72,55 @@ export default class InsightFacade implements IInsightFacade {
         // TODO: AL - Test whether string is blankspace, field is underscored or ID exists (COMPLETED)
         // return Promise.reject("Not implemented.");
     }
+    private createFileReadPromises(data: any): any[] {
+        let promisesFiles: any[] = [];
+        for (let file in data.files) {
+            if (file.length > "courses/".length && file.substring(0, 8) === "courses/") {
+                let currFilePromise = data.files[file].async("text")
+                    .then((courseData: any) => {
+                        return JSON.parse(courseData);
+                    })
+                    .catch((errZip: any) => {
+                        throw new InsightError("JSZip cannot parse the contents of a file");
+                    });
+                promisesFiles.push(currFilePromise);
+            }
+        }
+        return promisesFiles;
+    }
 
-    // private addDatasetHelper(promisesFile: Array<Promise<{}>>): Promise<string[]> {
-    //     return new Promise<string[]>((resolve, reject) => {
-    //
-    //     });
-    // }
+    private createValidSection(section: any) {
+        if (section.Subject && section.Course &&
+            section.Avg  && section.Professor && section.Title
+            && section.Pass && section.Fail && section.Audit
+            && section.id  && section.Year) {
+            let sectionObject: Section = new Section(section.Subject,
+                section.Course, section.Avg,
+                section.Professor, section.Title, section.Pass,
+                section.Fail, section.Audit, section.id,
+                section.Year);
+            return sectionObject;
+        }
+        return null;
+    }
 
+    private loadDatasetDisk(filename: string) {
+        let fileContent = fs.readFileSync(this.dataDir + filename, "utf8");
+        let currDatasetRead: DataSet = JSON.parse(fileContent);
+        this.datasets.addDataset(currDatasetRead);
+    }
+    private addDatasetDisk(currDataset: DataSet) {
+        this.datasets.addDataset(currDataset);
+        try {
+            if (!fs.existsSync(this.dataDir)) {
+                fs.mkdirSync(this.dataDir);
+            }
+            fs.writeFileSync(this.dataDir + currDataset.id,
+                JSON.stringify(currDataset));
+        } catch (e) {
+            throw new InsightError("Cannot write to disk");
+        }
+    }
     private isIDValid(id: string): boolean {
         // TODO : GET THE REGEX EQUIVALENT
         return (!(id.includes(" ")) && !(id.includes("_")));
