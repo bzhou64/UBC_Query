a9a70ad3b78b9acbc508439dacf1a21d0bd24791
raghavaggsss <zacefron.efron727@gmail.com>
2019-10-28 00:19:21 -0700

    fix section overall year

--- src/controller/DataHelpers.ts
+++ src/controller/DataHelpers.ts
@@ -10,17 +10,14 @@ function createValidSection(section: any) {
         section.Avg !== undefined && section.Professor !== undefined && section.Title !== undefined
         && section.Pass !== undefined && section.Fail !== undefined && section.Audit !== undefined
         && section.id !== undefined  && section.Year !== undefined) {
-        let year = 1900;
-        if (section.Year === "overall") {
-            year = 1900;
-        } else {
-            year = parseInt(section.Year, 10);
+        if (section.Section === "overall") {
+            section.Year = 1900;
         }
         let sectionObject: Section = new Section(section.Subject.toString(),
             section.Course.toString(), parseFloat(section.Avg),
             section.Professor.toString(), section.Title.toString(), parseInt(section.Pass, 10),
             parseInt(section.Fail, 10), parseInt(section.Audit, 10), section.id.toString(),
-            year);
+            parseInt(section.Year, 10));
         return sectionObject;
     }
     return null;
