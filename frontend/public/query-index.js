f432a4c4b37751b5793d6914cf00e20115f3cb25
raghavaggsss <zacefron.efron727@gmail.com>
2019-11-15 19:56:17 -0800

    complete query-sender

--- frontend/public/query-index.js
+++ frontend/public/query-index.js
@@ -8,4 +8,11 @@
  * 4.) Result is rendered in the reference UI by calling CampusExplorer.renderResult with the response from the endpoint as argument
  */
 
-// TODO: implement!
+document.getElementById("submit-button").onclick = bindSubmit;
+
+function bindSubmit() {
+    let query = CampusExplorer.buildQuery();
+    CampusExplorer.sendQuery(query).then((response) => {
+        console.log(response);
+    })
+}
--- frontend/public/query-sender.js
+++ frontend/public/query-sender.js
@@ -4,9 +4,18 @@
  * @param query The query object
  * @returns {Promise} Promise that must be fulfilled if the Ajax request is successful and be rejected otherwise.
  */
-CampusExplorer.sendQuery = function(query) {
-    return new Promise(function(fulfill, reject) {
-        // TODO: implement!
-        console.log("CampusExplorer.sendQuery not implemented yet.");
+CampusExplorer.sendQuery = function (query) {
+    return new Promise(function (fulfill, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST", '/post', true);
+        xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        xhr.onreadystatechange = function () { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                fulfill(xhr.response);
+            } else {
+                reject();
+            }
+        };
+        xhr.send(JSON.stringify(query));
     });
 };
