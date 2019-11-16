4ea9685b4ffa3bb5853832a13319190aabe700a1
raghavaggsss <zacefron.efron727@gmail.com>
2019-11-15 19:30:51 -0800

    complete query-builder

--- frontend/public/query-builder.js
+++ frontend/public/query-builder.js
@@ -5,9 +5,147 @@
  *
  * @returns query object adhering to the query EBNF
  */
-CampusExplorer.buildQuery = function() {
+
+coursesColumns = ["audit", "avg", "dept", "fail", "id", "instructor", "pass", "title", "uuid", "year"];
+roomsColumns = ["address", "fullname", "furniture", "href", "lat", "lon", "name", "number", "seats", "shortname", "type"];
+
+CampusExplorer.buildQuery = function () {
     let query = {};
-    // TODO: implement!
-    console.log("CampusExplorer.buildQuery not implemented yet.");
+    let dataSetKind = document.getElementsByClassName("tab-panel active")[0].id.split("-")[1];
+    query["WHERE"] = buildWhere(dataSetKind);
+    let order = buildOrder(dataSetKind);
+    let columns = buildColumns(dataSetKind);
+    if (order.keys.length > 0) {
+        query["OPTIONS"] = {COLUMNS: columns, ORDER: order};
+    } else {
+        query["OPTIONS"] = {COLUMNS: columns};
+    }
+    let groups = buildGroup(dataSetKind);
+    if (groups.length > 0) {
+        query["TRANSFORMATIONS"] = {GROUP: groups, APPLY: buildApply(dataSetKind)};
+    }
     return query;
 };
+
+function buildApply(dataSetKind) {
+    let applyArray = [];
+    let tabDatasetKind = document.getElementById("tab-" + dataSetKind);
+    let applyContainer = tabDatasetKind.getElementsByClassName("transformations-container")[0];
+    let applyElements = applyContainer.getElementsByClassName("control-group transformation");
+    for (let apply of applyElements) {
+        let term = apply.getElementsByClassName("control term")[0].getElementsByTagName("input")[0].value;
+        let operator = apply.getElementsByClassName("control operators")[0].getElementsByTagName("select")[0].value;
+        let field = apply.getElementsByClassName("control fields")[0].getElementsByTagName("select")[0].value;
+        let applyJson = {[term]: {[operator]: dataSetKind + "_" + field}};
+        applyArray.push(applyJson);
+    }
+    return applyArray;
+}
+
+function buildGroup(dataSetKind) {
+    let groups = [];
+    let groupIds = [];
+    if (dataSetKind === "courses") {
+        groupIds = coursesColumns;
+    } else {
+        groupIds = roomsColumns;
+    }
+    groupIds.forEach((colId) => {
+        let currElement = document.getElementById(dataSetKind + "-groups-field-" + colId);
+        if (currElement.checked === true) {
+            groups.push(dataSetKind + "_" + currElement.value)
+        }
+    });
+    return groups;
+}
+
+function buildOrder(dataSetKind) {
+    let keys = [];
+    let tabDatasetKind = document.getElementById("tab-" + dataSetKind);
+    let orderDiv = tabDatasetKind.getElementsByClassName("form-group order")[0];
+    let selectTagOptions = orderDiv.getElementsByTagName("select")[0].selectedOptions;
+    for (let option of selectTagOptions) {
+        keys.push(dataSetKind + "_" + option.value)
+    }
+    let isDescending = document.getElementById("courses-order").checked;
+    let order = {keys: keys};
+    if (isDescending === true) {
+        order["dir"] = "DOWN";
+    } else {
+        order["dir"] = "UP";
+    }
+    return order;
+}
+
+function buildColumns(dataSetKind) {
+    let columns = [];
+    let columnIds = [];
+    if (dataSetKind === "courses") {
+        columnIds = coursesColumns;
+    } else {
+        columnIds = roomsColumns;
+    }
+    columnIds.forEach((colId) => {
+        let currElement = document.getElementById(dataSetKind + "-columns-field-" + colId);
+        if (currElement.checked === true) {
+            columns.push(dataSetKind + "_" + currElement.value)
+        }
+    });
+    return columns;
+}
+
+function buildWhere(dataSetKind) {
+    let where = {};
+    let tabDatasetKind = document.getElementById("tab-" + dataSetKind);
+    let conditionRadios = {
+        AND: "courses-conditiontype-all",
+        OR: "courses-conditiontype-any", NONE: "courses-conditiontype-none"
+    };
+    let condition = "";
+
+    for (let cond of Object.keys(conditionRadios)) {
+        let condElement = document.getElementById(conditionRadios[cond]);
+        if (condElement.checked === true) {
+            condition = cond;
+        }
+    }
+    let conditionsContainer = tabDatasetKind.getElementsByClassName("conditions-container")[0];
+    let conditions = conditionsContainer.getElementsByClassName("control-group condition");
+    if (condition === "NONE" || conditions.length === 0) {
+        where = {}
+    } else {
+        let conditionsArray = formatConditions(conditions, dataSetKind);
+        if (conditions.length === 1) {
+            where = conditionsArray[0];
+        } else {
+            where = {[condition]: conditionsArray}
+        }
+    }
+    return where;
+}
+
+function formatConditions(conditions, datasetKind) {
+    let condArray = [];
+    for (let cond of conditions) {
+        let not = cond.getElementsByClassName("control not")[0].getElementsByTagName("input")[0].checked;
+        let field = cond.getElementsByClassName("control fields")[0].getElementsByTagName("select")[0].value;
+        let operator = cond.getElementsByClassName("control operators")[0].getElementsByTagName("select")[0].value;
+        let term = cond.getElementsByClassName("control term")[0].getElementsByTagName("input")[0].value;
+
+        if (operator === "EQ" || operator === "GT" || operator === "LT") {
+            term = parseInt(term, 10);
+        }
+
+        let conditionJson = {};
+        let conditionInner = {};
+        conditionInner[datasetKind + "_" + field] = term;
+        conditionJson[operator] = conditionInner;
+        if (not === true) {
+            conditionJson = {NOT: conditionJson};
+        }
+        condArray.push(conditionJson);
+    }
+    return condArray;
+}
+
+
